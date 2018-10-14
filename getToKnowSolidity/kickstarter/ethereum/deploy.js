31283174eb45fa36781ef0187586fbd08db8b73e
Adrianacmy <iamadrianachen@gmail.com>
2018-10-13 23:57:16 -0700

    deploy script can not be fixed

--- getToKnowSolidity/kickstarter/ethereum/deploy.js
+++ getToKnowSolidity/kickstarter/ethereum/deploy.js
@@ -1,7 +1,11 @@
 const HDwalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+// const { interface, bytecode } = require('./compile');
 const ganache = require('ganache-cli');
+
+const comipledFactory = require('./build/CampaignFactory.json');
+// const comipledCampaign = require('../ethereum/build/Compaign.json');
+
 
 // a mnemonic could generat mutiple address
 const provider = new HDwalletProvider(
@@ -9,18 +13,18 @@ const provider = new HDwalletProvider(
     'https://rinkeby.infura.io/v3/4d94e36f580841ad825b443f32c797cb'
 );
 
-const web3 = new Web3(ganache.provider());
-// const web3 = new Web3(provider);
+// const web3 = new Web3(ganache.provider());
+const web3 = new Web3(provider);
 
 //can not use async await outside of a function
 const deploy = async () => {
     const accounts = await web3.eth.getAccounts();
     console.log(accounts);
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode})
-    .send({from: accounts[0], gas: '6000000'});
+    const result = await new web3.eth.Contract(JSON.parse(comipledFactory.interface))
+        .deploy({ data: comipledFactory.bytecode })
+        .send({ from: accounts[0], gas: '1000000' });
 
-    console.log(interface)
+    // console.log(comipledFactory.interface)
     console.log(result.options.address);
 
 }
